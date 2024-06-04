8454fddf29bc3b2e8907ea6c2d66dc8783cbe483
MarceloJunior412005 <161842201+MarceloJunior412005@users.noreply.github.com>
2024-06-04 10:59:27 -0300

    Aula 11 JS

---
+++ ex008.js
@@ -0,0 +1,13 @@
+var vel = n
+console.log(`A velocidade do seu carro ${vel}Km/h`)
+if(vel > 60.5){
+    
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO !!!`)
+
+}else{
+    
+    console.log(`Dirija sempre usando cinto de segurança !`)
+}
+
+
+
