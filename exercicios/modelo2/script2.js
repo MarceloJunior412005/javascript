47ee709db7e26d4772b53a8c80d915515b28f44c
MarceloJunior412005 <161842201+MarceloJunior412005@users.noreply.github.com>
2024-06-21 08:29:48 -0300

    Módulo D js

--- exercicios/modelo2/script2.js
+++ exercicios/modelo2/script2.js
@@ -1,16 +1,14 @@
-function carregar() {
-    var msg = window.document.getElementById(`msg`)
-    var img = window.document.getElementById(`imagem`)
+function carregar(){
+    var msg = document.getElementById (`msg`)
+    var img = document.getElementById(`imagem`)
     var data = new Date()
     var hora = data.getHours()
     
-    
-
     msg.innerHTML = `Agora são ${hora} horas`
 
     if(hora >= 0 && hora < 12){
-        img.src = 'manha.png'
-        document.body.style.background = '#f5bd72'
+         img.src = 'manha.png'
+         document.body.style.background = '#f5bd72'
     }else if(hora >= 12 && hora < 18){
         img.src = 'tarde.png'
         document.body.style.background = '#f76602'
@@ -18,5 +16,4 @@ function carregar() {
         img.src = 'noite.png'
         document.body.style.background = '#0b1542'
     }
-
 }
