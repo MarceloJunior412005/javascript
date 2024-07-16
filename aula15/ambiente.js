3735017eeeb9829f6d4d169c5fe29d904edca390
MarceloJunior412005 <161842201+MarceloJunior412005@users.noreply.github.com>
2024-07-15 22:05:27 -0300

    Aula Vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,5 @@
+var num = [5,8,2,9,3]
+num[5] = 87
+
+
+console.log(`Nosso vetor é ${num}`)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,9 @@
+var num = [8,1,7,2,9]
+
+var buscar = num.indexOf(5)
+
+console.log(`O valor 5 está na posição ${buscar}` )
+
+/*for(var pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o  valor ${valores[pos]}`)
+}*/
